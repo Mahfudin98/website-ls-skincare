2767e4ac01583375d4fcaee08e147cb1739654d3
Mahfudin98 <mahfudinnawawi@gmail.com>
2023-05-02 12:05:46 +0700

    fix title home about

--- src/components/Pages/Home/About.tsx
+++ src/components/Pages/Home/About.tsx
@@ -6,13 +6,17 @@ export default function AboutHome() {
     <section className={styles["section-def"]}>
       <div className="grid grid-cols-12 gap-6 justify-items-center">
         <div className="col-span-12 md:col-span-6 lg:col-span-3">
-          <div className="w-[250px] h-[250px] bg-pic-900 rounded-full"></div>
+          <div className="w-[250px] h-[250px] bg-pic-900 rounded-full flex justify-center items-center">
+            <h2 className="text-white uppercase text-3xl font-semibold font-poppins">
+              About Us
+            </h2>
+          </div>
         </div>
         <div className="col-span-12 md:col-span-6 lg:col-span-3">
           <div className="w-full h-full bg-pic-100 p-6 rounded-3xl border border-brown-300">
             <div className="w-[90px] h-[90px] bg-brown-900 rounded-full mb-3"></div>
             <h3 className={`${styles["h3-title"]} group`}>
-              Skin care
+              Brand Story
               <ArrowRightCircleIcon
                 className={`${styles["h3-icon"]} group-hover:ml-1.5 group-hover:fill-brown-800 group-hover:text-pic-800`}
               />
@@ -27,7 +31,7 @@ export default function AboutHome() {
           <div className="w-full h-full bg-pic-100 p-6 rounded-3xl border border-brown-300">
             <div className="w-[90px] h-[90px] bg-brown-900 rounded-full mb-3"></div>
             <h3 className={`${styles["h3-title"]} group`}>
-              Skin care
+              All Brand
               <ArrowRightCircleIcon
                 className={`${styles["h3-icon"]} group-hover:ml-1.5 group-hover:fill-brown-800 group-hover:text-pic-800`}
               />
@@ -42,7 +46,7 @@ export default function AboutHome() {
           <div className="w-full h-full bg-pic-100 p-6 rounded-3xl border border-brown-300">
             <div className="w-[90px] h-[90px] bg-brown-900 rounded-full mb-3"></div>
             <h3 className={`${styles["h3-title"]} group`}>
-              Skin care
+              LS Aesthetic Clinic
               <ArrowRightCircleIcon
                 className={`${styles["h3-icon"]} group-hover:ml-1.5 group-hover:fill-brown-800 group-hover:text-pic-800`}
               />
