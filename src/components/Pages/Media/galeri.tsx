236b9e1323c6bf71e5b7952135bd266e89e9bf4a
Mahfudin98 <mahfudinnawawi@gmail.com>
2023-05-05 16:37:25 +0700

    add image to galeri

--- src/components/Pages/Media/galeri.tsx
+++ src/components/Pages/Media/galeri.tsx
@@ -1,5 +1,6 @@
 import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
 import styles from "../Home/home.module.css";
+import Image from "next/image";
 export default function Galeri() {
   return (
     <>
@@ -23,16 +24,40 @@ export default function Galeri() {
             <div className="h-16 text-sm font-semibold md:text-lg md:h-32 text-brown-900 md:col-start-2 md:row-start-2 md:flex">
               <span className="self-center md:self-end">@lsskincarepusat</span>
             </div>
-            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
-            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg md:col-start-3"></div>
-            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
-            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
-            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
-            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg md:col-start-2"></div>
-            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg md:col-start-4"></div>
-            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
-            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg md:col-start-3"></div>
-            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg md:col-start-3"></div>
+            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg relative overflow-hidden">
+              <Image
+                src="https://lh3.googleusercontent.com/d/1Ix5Cwsnpl_Q1KfiArrUSrKT7G8IREfDO?authuser=0"
+                height={720}
+                width={1280}
+                alt="image1"
+                className="object-cover h-32 duration-300 ease-in-out delay-75 hover:scale-105"
+              />
+            </div>
+            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg relative overflow-hidden md:col-start-3">
+              <Image
+                src="https://lh3.googleusercontent.com/d/15BsxMzDJGQSFfejd1eJ_846Itn0SWY1s?authuser=0"
+                height={720}
+                width={1280}
+                alt="image1"
+                className="object-cover h-32 duration-300 ease-in-out delay-75 hover:scale-105"
+              />
+            </div>
+            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg relative overflow-hidden">
+              <Image
+                src="https://lh3.googleusercontent.com/d/1B4m8J7CblwSkHx1yVyrpPSsrIKGdrcJg?authuser=0"
+                height={720}
+                width={1280}
+                alt="image1"
+                className="object-cover h-32 duration-300 ease-in-out delay-75 hover:scale-105"
+              />
+            </div>
+            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg relative overflow-hidden"></div>
+            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg relative overflow-hidden"></div>
+            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg relative overflow-hidden md:col-start-2"></div>
+            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg relative overflow-hidden md:col-start-4"></div>
+            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg relative overflow-hidden"></div>
+            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg relative overflow-hidden md:col-start-3"></div>
+            <div className="h-32 w-full bg-pic-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg relative overflow-hidden md:col-start-3"></div>
           </div>
         </label>
       </label>
