2a754717f353a6dce102658599d2c5d37163a819
Mahfudin98 <mahfudinnawawi@gmail.com>
2023-06-24 16:39:45 +0700

    update map view realtime set center

--- src/components/Pages/Seller/map_view.tsx
+++ src/components/Pages/Seller/map_view.tsx
@@ -1,30 +1,23 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useEffect, useRef, useState } from "react";
-import "leaflet/dist/leaflet.css";
+import { useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
 export default function MapView(props: any) {
   const { token, location } = props;
+  mapboxgl.accessToken = token;
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://api.mapbox.com/mapbox-gl-js/v2.4.0/mapbox-gl.js";
-    script.defer = true;
-    document.head.appendChild(script);
+    const map = new mapboxgl.Map({
+      container: "map-container",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [location.longitude, location.latitude],
+      zoom: 12
+    });
 
-    script.onload = () => {
-      window.mapboxgl.accessToken = token;
-
-      // Inisialisasi peta
-      const map = new window.mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/streets-v11", // Ganti dengan style peta yang diinginkan
-        center: [location.longitude, location.latitude], // Koordinat pusat peta [longitude, latitude]
-        zoom: 10 // Tingkat zoom peta
-      });
-
-      // Tambahkan marker
-      new window.mapboxgl.Marker()
-        .setLngLat([location.longitude, location.latitude])
-        .addTo(map);
+    const markerInstance = new mapboxgl.Marker()
+      .setLngLat([location.longitude, location.latitude])
+      .addTo(map);
+    return () => {
+      map.remove();
     };
-  }, [location.latitude, location.longitude, token]);
-  return <div id="map" style={{ height: "600px", width: "100%" }}></div>;
+  }, [location]);
+  return <div id="map-container" style={{ width: "100%", height: "600px" }} />;
 }
