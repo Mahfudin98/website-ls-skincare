0b413c0ef9ed9ae13a3748f56414bfefcbe52e5b
Mahfudin98 <mahfudinnawawi@gmail.com>
2023-07-31 10:04:08 +0700

    add google analytyc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,48 @@
+/* eslint-disable @next/next/next-script-for-ga */
+/* eslint-disable @next/next/no-head-element */
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head />
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-KR8FTBK9');
+              `
+          }}
+        />
+        <Head />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-S38JF4SPBK"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-S38JF4SPBK');
+              `
+          }}
+        />
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KR8FTBK9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
